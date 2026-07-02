d9eff47fdeb271ca0ae7667fd38843efa4240c2b
Doitwel7 <akinolaidris2001@gmail.com>
2026-07-02 15:17:33 +0100

    Add GitHub Pages workflow

--- sunbeth/vite.config.ts
+++ sunbeth/vite.config.ts
@@ -1,13 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite' // Add this import
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/sunbeth/',
+
   plugins: [
     vue(),
-    tailwindcss(), // Add this here so Tailwind 4 actually works!
+    tailwindcss(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
